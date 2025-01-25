6d388663a0ae66e9110c5e12d37c69926e8fb1ac
Niah Price <nrprice2@outlook.com>
2025-01-25 22:12:20 +0000

    bugs and fixes

--- QuizApp/src/components/progressTracker.js
+++ QuizApp/src/components/progressTracker.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { questions } from "../questions";
 
 const ProgressTracker = ({ currentQIndex, totalQ }) => (
     <div id="progress-tracker" style={{ margin: '20px 0' }}>Question {currentQIndex + 1} of {totalQ}</div>
